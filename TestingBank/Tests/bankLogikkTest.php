42a3e0c37e6fa8c495d7da351af2a11d47c593c7
goEivindA <43267006+goEivindA@users.noreply.github.com>
2020-01-30 15:10:49 +0100

    Add files via upload

--- TestingBank/Tests/bankLogikkTest.php
+++ TestingBank/Tests/bankLogikkTest.php
@@ -28,4 +28,54 @@ class bankLogikkTest extends PHPUnit\Framework\TestCase {
         $konto = $bank->hentKonti($personnummer);
         $this->assertEquals(" ", $konto[0]);
     }
+    
+    public function testHentSaldi(){
+        $personnummer = "01010122344";
+        
+        $bank = new Bank(new BankDBStub());
+        $konto = $bank->hentSaldi($personnummer);
+        $this->assertEquals("105010123456", $konto[0]);
+        $this->assertEquals("01010122344", $konto[1]);
+        $this->assertEquals("720", $konto[2]);
+        $this->assertEquals("Lønnskonto", $konto[3]);
+        $this->assertEquals("NOK", $konto[4]);
+    }
+    
+    public function testHentSaldiBlank(){
+        $personnummer = "01010122355";
+        
+        $bank = new Bank(new BankDBStub());
+        $konto = $bank->hentSaldi($personnummer);
+        $this->assertEquals(" ", $konto[0]);
+    }
+    
+    public function testRegistrerBetaling(){
+        $kontonummer = "105010123456";
+        $transaksjon = new transaksjon;
+        $transaksjon -> belop = 20;
+        
+        $bank = new Bank(new BankDBStub);
+        $betaling = $bank->registrerBetaling($kontonummer, $transaksjon);
+        $this->assertEquals("OK", $betaling);
+    }
+    
+    public function testRegistrerBetalingBelopFeil(){
+        $kontonummer = "105010123456";
+        $transaksjon = new transaksjon;
+        $transaksjon -> belop = 0;
+        
+        $bank = new Bank(new BankDBStub);
+        $betaling = $bank->registrerBetaling($kontonummer, $transaksjon);
+        $this->assertEquals("Feil", $betaling);
+    }
+    
+    public function testRegistrerBetalingKontoNrFeil(){
+        $kontonummer = "105010123477";
+        $transaksjon = new transaksjon;
+        $transaksjon -> belop = 20;
+        
+        $bank = new Bank(new BankDBStub);
+        $betaling = $bank->registrerBetaling($kontonummer, $transaksjon);
+        $this->assertEquals("Feil", $betaling);
+    }
 }
