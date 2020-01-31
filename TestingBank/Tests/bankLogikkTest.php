6956bb7dd740832471ec327823ef713be13e90cf
goEivindA <43267006+goEivindA@users.noreply.github.com>
2020-01-31 12:31:33 +0100

    Add files via upload

--- TestingBank/Tests/bankLogikkTest.php
+++ TestingBank/Tests/bankLogikkTest.php
@@ -78,4 +78,117 @@ class bankLogikkTest extends PHPUnit\Framework\TestCase {
         $betaling = $bank->registrerBetaling($kontonummer, $transaksjon);
         $this->assertEquals("Feil", $betaling);
     }
+    
+    public function testHentBetalingerFlere(){
+        $personnummer = "01010122344";
+        
+        $bank = new Bank(new BankDBStub);
+        $betaling = $bank->hentBetalinger($personnummer);
+        $this->assertEquals("105010123456", $betaling[0]);
+        $this->assertEquals("205010123456", $betaling[1]);
+    }
+    
+    public function testHentBetalingerEn(){
+        $personnummer = "01010122355";
+        
+        $bank = new Bank(new BankDBStub);
+        $betaling = $bank->hentBetalinger($personnummer);
+        $this->assertEquals("505010123456", $betaling[0]);
+    }
+    
+    public function testHentBetalingerBlank(){
+        $personnummer = "01010122377";
+        
+        $bank = new Bank(new BankDBStub);
+        $betaling = $bank->hentBetalinger($personnummer);
+        $this->assertEquals(" ", $betaling[0]);
+    }
+    
+    public function testUtforBetalingGodkjent(){
+        $TxID = "1";
+        
+        $bank = new Bank(new BankDBStub);
+        $utforBetaling = $bank->utforBetaling($TxID);
+        $this->assertEquals("OK", $utforBetaling);
+    }
+    
+    public function testUtforBetalingFlereTransaksjoner(){
+        $TxID = "2";
+        
+        $bank = new Bank(new BankDBStub);
+        $utforBetaling = $bank->utforBetaling($TxID);
+        $this->assertEquals("Feil", $utforBetaling);
+    }
+    
+    public function testUtforBetalingForLitePaaSaldo(){
+        $TxID = "3";
+        
+        $bank = new Bank(new BankDBStub);
+        $utforBetaling = $bank->utforBetaling($TxID);
+        $this->assertEquals("Feil", $utforBetaling);
+    }
+    
+    public function testUtforBetalingFinnerIkkeTransaksjon(){
+        $TxID = "4";
+        
+        $bank = new Bank(new BankDBStub);
+        $utforBetaling = $bank->utforBetaling($TxID);
+        $this->assertEquals("Feil", $utforBetaling);
+    }
+    
+    public function testendreKundeInfo(){
+        $kunde = new kunde();
+        $kunde->fornavn = "Kari";
+        $kunde->etternavn = "Hansen";
+        $kunde->postnr = "4455";
+        
+        $bank = new Bank(new BankDBStub);
+        $endreKunde = $bank->endreKundeInfo($kunde);
+        $this->assertEquals("OK", $endreKunde);
+    }
+    public function testendreKundeInfoNyttPoststed(){
+        $kunde = new kunde();
+        $kunde->fornavn = "Kari";
+        $kunde->etternavn = "Hansen";
+        $kunde->postnr = "2020";
+        
+        $bank = new Bank(new BankDBStub);
+        $endreKunde = $bank->endreKundeInfo($kunde);
+        $this->assertEquals("OK", $endreKunde);
+    }
+    public function testEndreKundeFeil(){
+        $kunde = new kunde();
+        $kunde->fornavn = "Hans";
+        $kunde->etternavn = "Hansen";
+        $kunde->postnr = "4455";
+        
+        $bank = new Bank(new BankDBStub);
+        $endreKunde = $bank->endreKundeInfo($kunde);
+        $this->assertEquals("Feil", $endreKunde);
+    }
+    
+    public function testHentKundeInfo(){
+        $personnummer = "01010122344";
+        
+        $bank = new Bank(new BankDBStub());
+        $kunde = $bank ->hentKundeInfo($personnummer);
+        $this->assertEquals("Kari", $kunde->fornavn);
+        $this->assertEquals("Hansen", $kunde->etternavn);
+    }
+    
+    public function testHentKundeInfoFeilIPostNr(){
+        $personnummer = "01010122355";
+        
+        $bank = new Bank(new BankDBStub());
+        $kunde = $bank ->hentKundeInfo($personnummer);
+        $this->assertEquals("Feil", $kunde);
+    }
+    
+    public function testHentKundeInfoFeilIPersonNr(){
+        $personnummer = "20010122355";
+        
+        $bank = new Bank(new BankDBStub());
+        $kunde = $bank ->hentKundeInfo($personnummer);
+        $this->assertEquals("Feil", $kunde);
+    }
 }
